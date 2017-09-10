59d38c6de49acf16ec4c055c8421d7abbf9d1219
clo2go <clo2go@gmail.com>
2017-09-09 21:21:01 -0500

    working on board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,18 @@
+const printBoard = board => {
+console.log('Current Board: ');
+console.log(board[0].join(' | '));
+console.log(board[1].join(' | '));
+console.log(board[2].join(' | '));
 
-const blankLine = `  |   |  `;
-const guessLine = `1 |   |  `;
-const bombLine  = `  | B |  `;
-console.log(`This is what an empty board would look like:`);
-console.log (blankLine);
-console.log (blankLine);
-console.log (blankLine);
-console.log(`This is what a board with a guess and a bomb on it would look like:`);
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+};
+
+const board = [
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
+
+printBoard(board);
+board[0][1] = '1';
+board[1][2] = '1';
+printBoard(board);
